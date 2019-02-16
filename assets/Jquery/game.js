ee930b2f0a03d603171aec951384f69b2870a189
Ricardo Quintanar <lrq1012@gmail.com>
2019-02-16 12:55:23 -0600

    characters have been added, constructor was created, second page set up, next is linking all players and attacks to work

--- assets/Jquery/game.js
+++ assets/Jquery/game.js
@@ -1,4 +1,4 @@
-// Global variables
+// Global variables //
 var baseAttack = 0;
 var player;
 var defender; 
@@ -6,7 +6,7 @@ var charArray = [];
 var playerSelected = false; 
 var defenderSelected = false;
 
-// Contruct(object design)
+// Contruct(charcater design of internal information ( strength attack counter and image of character)) //
 function Character(name, hp, ap, counter, pic) {
     this.name = name;
     this.healthPoints = hp;
@@ -14,14 +14,40 @@ function Character(name, hp, ap, counter, pic) {
     this.counterAttackPower = counter;
     this.pic = pic;
 }
+// function for Character initialization //
+function initCharacters() {
+    var gohan = new Character("Gohan",200, 60, 10, "assets/images/gohan1.gif")
+    var goku = new Character("Goku",250, 50, 35, "assets/images/gokuplayercard.gif")
+    var buu = new Character("Kid Buu",225, 40, 20,"assets/images/douchebuu.gif")
+    var cell = new Character("Cell",250, 35, 25, "assets/images/perfectcell.gif")
+    var frieza = new Character("Frieza",200, 30, 15, "assets/images/frieza.gif")
+    charArray.push(gohan, goku, buu, cell, frieza)
+    }
+    
+// Set character cards to pick from //
+function characterCards (divID) {
+    $(divID).children().remove();
+    for (var i = 0; i < charArray.length; i++) {
+        $(divID).append("<div />");
+        $(divID + " div:last-child").addClass("card");
+        $(divID + " div:last-child").append("<img />");
+        $(divID + " img:last-child").attr("id", charArray[i].name);
+        $(divID + " img:last-child").attr("class", "card-img-top");
+        $(divID + " img:last-child").attr("src", charArray[i].pic);
+        $(divID + " img:last-child").attr("width", 150);
+        $(divID + " img:last-child").addClass("img-thumbnail");
+        $(divID + " div:last-child").append(charArray[i].name + "<br>");
+        $(divID + " div:last-child").append("HP: " + charArray[i].healthPoints);
+        $(divID + " idv:last-child").append();
+    }
+}
 
-
-// Increase the attack strength (this attack points + original attack points)
+// Increase the attack strength (this attack points + original attack points) // 
 Character.prototype.increaseAttack = function () {
     this.attackPower += baseAttack;
 };
 
-// Performs an attack
+// Performs an attack // 
 Character.prototype.attack = function (Obj) {
     Obj.healthPoints -= this.attackPower;
     $("#msg").html("attacking" +
@@ -29,20 +55,37 @@ Character.prototype.attack = function (Obj) {
     this.increaseAttack();
 };
 
-// Performs a counter attack
+// Performs a counter attack //
 Character.prototype.counterAttack = function (Obj) {
     Obj.healthPoints -= this.counterAttackPower;
     $("#msg").append("<br>" + this.name + " counter attacked you for " + this.counterAttackPower + " damage points.");
 };
 
-function initCharacters() {
-var princess = new Character("Princess Leia","./assets/images/princess2.jpg")
-var yoda = new Character("Master Yoda","./assets/images/yoda1-1.jpg")
-var princess = new Character("Princess Leia","./assets/images/darthB.jpg")
-var princess = new Character("Princess Leia","./assets/images/Anakin-Skywalker-1.jpg")
-}
 
+// 
+// setting the on click an character to move it into user character chose and battlescreen//
+function updatePics(fromDivID, toDivID) {
+    $(fromDivID).children().remove();
+    for(var i = 0; i < charArray.length; i++) {
+        $(toDivID).append("<img />");
+        $(toDivID + " img:last-child").attr("src", charArray[i].name);
+        $(toDivID + " img:last-child").attr("src", charArray[i].pic);
+        $(toDivID + " img:last-child").attr("width", 150);
+        $(toDdivID + " img:last-child").addClass("img-thumbnail");
 
+    }
+};
+// $(document).on("click", "img", function () {
+//     if (playerSelected && !defenderSelected && (this.id != player.name)) {
+//         for (var c = 0; c < charArray.length; i++) {
+//             if (charArray[c].name == (this).id) {
+//                 defender.charArray [c];
+//                 charArray.splice[c , 1];
+//                 defenderSelected=true;
+
+//             }
+//         };
+        
 
 
 
@@ -54,3 +97,13 @@ function changeView(){
     $("#homescreen").empty();
     $("#battlescreen").show();
 }
+
+// Deploy //
+
+$(document).ready(function (){
+    $("#battlescreen").hide();
+    $("#globalMsg").hide();
+    initCharacters();
+    characterCards("#game");
+
+});
